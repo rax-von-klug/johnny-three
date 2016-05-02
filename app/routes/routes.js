5cfdeb1b918d55f2ec1f4f13601e785d5ccdf749
Christopher Lund <clund@connectionseducation.com>
2016-05-02 13:14:09 -0400

    fixing some bugs

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,114 +1,134 @@
-var requestify      = require('requestify');
-var slack           = require('../controllers/botkit');
-var util            = require('util');
+var Request          = require('request')
+var slack           = require('../controllers/botkit')
 
+// frontend routes =========================================================
 module.exports = function(app) {
 
-    app.get('/', function(req, res) {
-        console.log("root");
+  //public pages=============================================
+  //root
+  app.get('/', function(req, res) {
+    console.log("root")
 
-        res.render('root'); // load view/root.html file
-    });
+    res.render('root'); // load view/root.html file
+  });
 
-    app.get('/new', function(req, res) {
-        console.log("================== START TEAM REGISTRATION ==================")
-        
-        var auth_code = req.query.code;
+  //new user creation - redirection from Slack
+  app.get('/new', function(req, res) {
+    console.log("================== START TEAM REGISTRATION ==================")
+    //temporary authorization code
+    var auth_code = req.query.code
 
-        if (!auth_code) {
-            res.redirect('/');
+    if(!auth_code){
+      //user refused auth
+      res.redirect('/')
+    }
+    else{
+      console.log("New user auth code " + auth_code)
+      perform_auth(auth_code, res)
+    }
+  })
+
+  //CREATION ===================================================
+
+  var perform_auth = function(auth_code, res){
+    //post code, app ID, and app secret, to get token
+    var auth_adresse = 'https://slack.com/api/oauth.access?'
+    auth_adresse += 'client_id=' + process.env.SLACK_ID
+    auth_adresse += '&client_secret=' + process.env.SLACK_SECRET
+    auth_adresse += '&code=' + auth_code
+    auth_adresse += '&redirect_uri=' + process.env.SLACK_REDIRECT + "new"
+
+    Request.get(auth_adresse, function (error, response, body) {
+      if (error){
+        console.log(error)
+        res.sendStatus(500)
+      }
+
+      else{
+        var auth = JSON.parse(body)
+        console.log("New user auth")
+        console.log(auth)
+
+        register_team(auth,res)
+      }
+    })
+  }
+
+  var register_team = function(auth, res){
+    //first, get authenticating user ID
+    var url = 'https://slack.com/api/auth.test?'
+    url += 'token=' + auth.access_token
+
+    Request.get(url, function (error, response, body) {
+      if (error){
+        console.log(error)
+        res.sendStatus(500)
+      }
+      else{
+        try{
+          var identity = JSON.parse(body)
+          console.log(identity)
+
+          var team = {
+            id: identity.team_id,
+            bot:{
+              token: auth.bot.bot_access_token,
+              user_id: auth.bot.bot_user_id,
+              createdBy: identity.user_id
+            },
+            createdBy: identity.user_id,
+            url: identity.url,
+            name: identity.team
+          }
+          startBot(team)
+          res.send("Your bot has been installed")
+
+          saveUser(auth, identity)
+        }
+        catch(e){
+          console.log(e)
+        }
+      }
+    })
+  }
+
+  var startBot = function(team){
+    console.log(team.name + " start bot")
+
+    slack.connect(team)
+  }
+
+  var saveUser = function(auth, identity){
+    // what scopes did we get approved for?
+    var scopes = auth.scope.split(/\,/);
+
+    slack.controller.storage.users.get(identity.user_id, function(err, user) {
+      isnew = false;
+      if (!user) {
+          isnew = true;
+          user = {
+              id: identity.user_id,
+              access_token: auth.access_token,
+              scopes: scopes,
+              team_id: identity.team_id,
+              user: identity.user,
+          };
+      }
+      slack.controller.storage.users.save(user, function(err, id) {
+        if (err) {
+          console.log('An error occurred while saving a user: ', err);
+          slack.controller.trigger('error', [err]);
         }
         else {
-            console.log("New user auth code " + auth_code);
-            perform_auth(auth_code, res);
+          if (isnew) {
+            console.log("New user " + id.toString() + " saved");
+          }
+          else {
+            console.log("User " + id.toString() + " updated");
+          }
+          console.log("================== END TEAM REGISTRATION ==================")
         }
+      });
     });
-
-    var perform_auth = function(auth_code, res) {
-        var base_url = 'https://slack.com/api/oauth.access?client_id=%s&client_secret=%s&code=%s&redirect_uri=%snew';
-        var auth_adresse = util.format(base_url, process.env.SLACK_ID, process.env.SLACK_SECRET, auth_code, process.env.SLACK_REDIRECT);
-
-        console.log(auth_adresse);
-
-        requestify.get(auth_adresse).then((res) => {
-            var auth = JSON.parse(body);
-            console.log("New user auth");
-            console.log(auth);
-
-            register_team(auth, res);
-        });
-    }
-
-    var register_team = function(auth, res) {
-        var url = util.format('https://slack.com/api/auth.test?token=%s', auth.access_token);
-
-        requestify.get(url).then((res) => {
-        
-            try {
-                var identity = JSON.parse(body)
-                console.log(identity)
-
-                var team = {
-                    id: identity.team_id,
-                    bot:{
-                        token: auth.bot.bot_access_token,
-                        user_id: auth.bot.bot_user_id,
-                        createdBy: identity.user_id
-                    },
-                    createdBy: identity.user_id,
-                    url: identity.url,
-                    name: identity.team
-                }
-                
-                startBot(team);
-                res.send("Your bot has been installed")
-
-                saveUser(auth, identity)
-            }
-            catch (e) {
-                console.log(e);
-            }
-        });
-    }
-
-    var startBot = function(team) {
-        console.log(team.name + " start bot");
-
-        slack.connect(team);
-    }
-
-    var saveUser = function(auth, identity) {
-        var scopes = auth.scope.split(/\,/);
-
-        slack.controller.storage.users.get(identity.user_id, function(err, user) {
-            isnew = false;
-            
-            if (!user) {
-                isnew = true;
-                user = {
-                    id: identity.user_id,
-                    access_token: auth.access_token,
-                    scopes: scopes,
-                    team_id: identity.team_id,
-                    user: identity.user,
-                };
-            }
-            
-            slack.controller.storage.users.save(user, function(err, id) {
-                if (err) {
-                    console.log('An error occurred while saving a user: ', err);
-                    slack.controller.trigger('error', [err]);
-                }
-                else {
-                    if (isnew) {
-                        console.log("New user " + id.toString() + " saved");
-                    }
-                    else {
-                        console.log("User " + id.toString() + " updated");
-                    }
-                    console.log("================== END TEAM REGISTRATION ==================")
-                }
-            });
-        });
-    }
+  }
 }

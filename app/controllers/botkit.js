9e099484d1157751a2508772fb5c1a0a010d4a1c
Ashwin Chander <acchander@connectionseducation.com>
2016-06-09 20:24:39 -0400

    Add pull request event handler

--- app/controllers/botkit.js
+++ app/controllers/botkit.js
@@ -136,7 +136,7 @@ controller.storage.teams.all(function(err,teams) {
 // connect all teams with bots up to slack!
     for (var t  in teams) {
         if (teams[t].bot) {
-            var bot = controller.spawn(teams[t]).startRTM(function(err) {
+            var bot = controller.spawn(teams[t]).startRTM(function(err, bot) {
                 if (err) {
                     console.log('Error connecting bot to Slack:',err);
                 } else {

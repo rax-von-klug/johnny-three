0a19fb5b77f3245d75f4820f3e9c4e615e7421a6
Christopher Lund <clund@connectionseducation.com>
2016-12-08 22:17:33 -0500

    BB

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -45,6 +45,7 @@ module.exports = function(app) {
 
       if (payload.callback_id.includes("join_shared_channel_")) {
           slack.controller.storage.shares.get(payload.actions[0].value, function(err, shared) {
+              console.log(shared);
               slack.controller.storage.team.get(payload.team.id, function(err, team_data) {
                   if (!_.isArray(shared.joinedChannels)) {
                     shared.joinedChannels = [];
